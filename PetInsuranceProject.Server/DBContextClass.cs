464ab39578bb576fc5538dbf1389cb525162f356
Daigeona Guinn <LQ3571@corp.Alfacompanies.com>
2025-07-08 13:51:53 -0500

    feature/db-context: dbContext file created

---
+++ PetInsuranceProject.Server/DBContextClass.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace PetInsuranceProject.Server;
+
+    public class DBContextClass : DbContext
+{
+    DBContextClass(DbContextOptions<DBContextClass> options) : base(options)
+    {
+
+    }
+
+
+   // public DbSet<PetModel> Pet { get; set; }
+    
+
+
+
+}  
+
+
+
+
+
+
+
