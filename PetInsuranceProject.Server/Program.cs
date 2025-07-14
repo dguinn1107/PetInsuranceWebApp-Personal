989555b80d15f36ec0d55472e9d1666530973c36
Guinn, Daigeona C. <LQ3571@corp.Alfacompanies.com>
2025-07-14 17:04:48 -0500

    feature/quickquote-api: Enhance quick quote functionality and API integration
    - Updated `getQuote` in `QuickQuote.vue` to validate age and breed, and send structured requests to the new API endpoint.
    - Added new package references in `PetInsuranceProject.Server.csproj` for improved database management and code generation.
    - Modified `Program.cs` to include CORS policy, Swagger services, and map new quick quote endpoints, while commenting out Azure Key Vault configuration.
    - Introduced `QuickQuoteEndpoint.cs` to handle quote requests, including validation and premium calculation based on pet characteristics.

--- PetInsuranceProject.Server/Program.cs
+++ PetInsuranceProject.Server/Program.cs
@@ -7,32 +7,53 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.SqlServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
-
-
-
-
-
+using Scalar.AspNetCore;
+using Microsoft.AspNetCore.OpenApi;
+using PetInsuranceProject.Server.Endpoints;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
-var keyVaultName = builder.Configuration["KeyVaultName"];
-var keyVaultUri = new Uri($"https://{keyVaultName}.vault.azure.net/");
+//var keyVaultName = builder.Configuration["KeyVaultName"];
+//var keyVaultUri = new Uri($"https://{keyVaultName}.vault.azure.net/");
 
-builder.Configuration.AddAzureKeyVault(keyVaultUri, new DefaultAzureCredential());
-builder.Services.AddControllers();
-
+//builder.Configuration.AddAzureKeyVault(keyVaultUri, new DefaultAzureCredential());
 
 
 builder.Services.AddDbContext<PetInsuranceContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:58096") 
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
 
 
 var app = builder.Build();
 
-app.UseCors("AllowClient");
-app.MapControllers();
+app.UseCors(MyAllowSpecificOrigins);
+
+
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapQuickQuoteEndpoints();
 
 app.Run();
 
